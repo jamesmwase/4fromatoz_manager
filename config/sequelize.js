19046594e387437feb195bd5c24e2b7511071fb8
jamesmwase <53550640+jamesmwase@users.noreply.github.com>
2019-08-06 13:58:12 +0200

    Update sequelize.js

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 // Option 1: Passing parameters separately
-exports.sequelize = new Sequelize('fromatoz_root', 'root', 'DFLKYWrttddr#@(&%^$);<>?/3208645', {
+exports.sequelize = new Sequelize('fromatoz_root', 'root', 'skdi_^&)98w8HGAS', {
   host: 'localhost',
   dialect: 'mysql'
 });
