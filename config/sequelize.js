3092eac63da238fc95cf86d30f4e1993e889d659
jamesmwase <53550640+jamesmwase@users.noreply.github.com>
2019-08-07 09:57:22 +0200

    Update sequelize.js

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,6 +1,21 @@
 const Sequelize = require('sequelize');
 
 // Option 1: Passing parameters separately
+switch (process.env.NODE_ENV) {
+  case 'production':
+    console.log(process.env.NODE_ENV);
+    break;
+  case 'development':
+    console.log(process.env.NODE_ENV);
+    break;
+  case 'test':
+    console.log(process.env.NODE_ENV);
+    break;
+  default:
+    console.log(process.env.NODE_ENV);
+    break;
+
+}
 exports.sequelize = new Sequelize('fromatoz_root', 'root', 'skdi_^&)98w8HGAS', {
   host: 'localhost',
   dialect: 'mysql'
